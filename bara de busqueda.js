e167f2099811d33be1ad9b11dd0b31aa02e2797b
Nigel <nigelarmato@gmail.com>
2025-02-27 03:10:24 -0300

    se han varios cambios, se ha modificado la busqueda de la barra de busqueda, tambiens e han agregado las paginas de el boton de despliegue

--- bara de busqueda.js
+++ bara de busqueda.js
@@ -6,17 +6,44 @@ document.addEventListener("DOMContentLoaded", function () { //!Es un evento que 
 
         // Diccionario con las rutas de cada página
         let paginas = {
-            "notebooks": "Notebooks.html",
-            "celulares": "#",
-            "heladeras": "#",
-            "consolas": "#",
-            "televisores": "#"
+            "notebook": "Notebook.html",
+            "Notebook": "Notebook.html",
+            "HP": "Notebook.html",
+            "hp": "Notebook.html",
+            "Lenovo": "Notebook.html",
+            "lenovo": "Notebook.html",
+            "celulares": "celulares.html",
+            "Celulares": "celulares.html",
+            "Motorola": "celulares.html",
+            "motorola": "celulares.html",
+            "Samsung": "celulares.html",
+            "samsung": "celulares.html",
+            "sillas": "sillas.html",
+            "Sillas": "sillas.html",
+            "Noblechair": "sillas.html",
+            "noblechair": "sillas.html",
+            "Sillas Gamer": "sillas.html",
+            "sillas gamer": "sillas.html",
+            "Vertagear": "sillas.html",
+            "vertagear": "sillas.html",
+            "consolas": "consola.html",
+            "Consolas": "consola.html",
+            "xbox": "consola.html",
+            "retro": "consola.html",
+            "consolas retro": "consola.html",
+            "Consolas Retro": "consola.html",
+            "consola retro": "consola.html",
+            "Consola Retro": "consola.html",
+            "miniconsolas": "consola.html",
+            "televisores": "televisores.html",
+            "Televisores": "televisores.html",
+            "samsung": "televisores.html",
         };
 
         if (paginas[input]) {
             window.location.href = paginas[input]; // Redirige a la página correspondiente
         } else {
-            alert("No se encontraron resultados. Prueba con: Notebooks, Consolas, etc.");
+            alert("No se encontraron resultados. Prueba con: Notebook, Consolas, etc.");
         }
     });
 });
